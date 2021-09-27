c5048ad58f7d581c635ced2ee931fb71082fae8b
sharath V <sharathkrml@gmail.com>
2021-09-27 16:54:24 +0530

    ui modifed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import './App.css';
-import Bottomheader from './components/BottomHeader/Bottomheader';
-import Comments from './components/comments/Comments';
-import NavBar from './components/NavBar/NavBar';
-
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Bottomheader from "./components/BottomHeader/Bottomheader";
+import Comments from "./components/comments/Comments";
+import NavBar from "./components/NavBar/NavBar";
+const axios = require("axios");
 function App() {
-  const msg = "Labore nisi dolor adipisicing id duis reprehenderit. Do aliquip quis cupidatat sit occaecat do esse pariatur sint nostrud eiusmod aliqua quis. Voluptate reprehenderit quis ut reprehenderit. Tempor reprehenderit excepteur irure aute in."
+  const [results, setResults] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        "http://127.0.0.1:8000?page=2&order=desc&sort=activity&q=python3&site=stackoverflow"
+      )
+      .then((res) => {
+        setResults(res.data.items);
+        console.log(res.data.items);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .then(() => {
+        console.log("axios called");
+      });
+  }, []);
+
   return (
     <div className="App">
       <NavBar />
       <Bottomheader />
-    
+
       <div className="commentContainer">
-      <Comments username="username" date="date" message={msg} />
-      <Comments username="username" date="date" message={msg} />
-      <Comments username="username" date="date" message={msg} />
-      <Comments username="username" date="date" message={msg} />
-      <Comments username="username" date="date" message={msg} />
-      <Comments username="username" date="date" message={msg} />
-      <Comments username="username" date="date" message={msg} />
-      <Comments username="username" date="date" message={msg} />
-      <Comments username="username" date="date" message={msg} />
-      <Comments username="username" date="date" message={msg} />
+        {results.map((result, index) =>
+        (<Comments
+          key={index}
+          link={result.link}
+          username={result.owner.display_name}
+          message={result.title}
+        />))}
+        <div class="pagination">
+          <div>&laquo;</div>
+          <div>1</div>
+          <div class="active" href="#">2</div>
+          <div href="#">3</div>
+          <div href="#">4</div>
+          <div href="#">5</div>
+          <div href="#">6</div>
+          <div href="#">&raquo;</div>
+        </div>
       </div>
     </div>
   );
--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -2,15 +2,17 @@ import React from 'react'
 import './comments.css'
 function Comments(props) {
     return (
+        <a style={{textDecoration:'none'}} href={props.link}>
+
         <div className="comment">
             <div className="content">
              <p>{props.message}</p>
             </div>
             <div className="bottom-section">
-             <span className="date">{props.date}</span>
              <span className="username">{props.username}</span>
             </div>
         </div>
+        </a>
     )
 }
 export default Comments
